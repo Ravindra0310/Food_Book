b8b9a36c8e51ac951851043875a7a2a5a21a7a19
PaPluckfii <sumeet1996das@gmail.com>
2021-06-26 17:34:41 +0530

    post feature done UI improvements left

---
+++ app/src/main/java/com/ravi/foodbook/ui/camera/CameraFragment.kt
@@ -0,0 +1,22 @@
+package com.ravi.foodbook.ui.camera
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ravi.foodbook.R
+
+
+class CameraFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_camera, container, false)
+    }
+
+
+}
--- app/src/main/java/com/ravi/foodbook/ui/post/PostViewModel.kt
+++ app/src/main/java/com/ravi/foodbook/ui/post/PostViewModel.kt
@@ -1,6 +1,33 @@
 package com.ravi.foodbook.ui.post
 
+import android.net.Uri
 import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.ravi.foodbook.model.dao.PostDao
+import com.ravi.foodbook.model.data.PostData
 
 class PostViewModel: ViewModel() {
+
+    private val auth = FirebaseAuth.getInstance()
+    private val database = FirebaseDatabase.getInstance()
+    private val postDatabaseReference = database.getReference("posts")
+
+    fun getUserId(): String{
+        return auth.currentUser?.uid!!
+    }
+
+    fun getUserName(): String {
+        return auth.currentUser?.displayName!!
+    }
+
+    fun getUserPic(): Uri {
+        return auth.currentUser?.photoUrl!!
+    }
+
+    fun addPost(data: PostData) {
+        val newPostRef = postDatabaseReference.push()
+        newPostRef.setValue(data)
+    }
+
 }
